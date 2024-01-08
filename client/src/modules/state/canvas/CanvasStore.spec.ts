ec63528242524f1509535c060dabd3488ceebd3c
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2024-01-08 13:45:25 +0530

    feat: added tests for other canvas store fns

--- client/src/modules/state/canvas/CanvasStore.spec.ts
+++ client/src/modules/state/canvas/CanvasStore.spec.ts
@@ -1,7 +1,10 @@
 import { describe, expect, it } from "vitest";
 import CanvasStore from "./CanvasStore";
 import { CAMERA_ANGLE, RECT_H, RECT_W } from "modules/core/constants";
-import { cameraToScreenCoordinates } from "modules/core/camera-utils";
+import {
+  cameraToScreenCoordinates,
+  scaleWithAnchorPoint,
+} from "modules/core/camera-utils";
 
 describe("CanvasStore", () => {
   it("should initialize the store", () => {
@@ -36,6 +39,48 @@ describe("CanvasStore", () => {
   it("should calculate scale", () => {
     let canvas = new CanvasStore();
     canvas.initialize(1000, 1000);
-    expect(canvas.scale).toBe(1);
+    expect(canvas.scale.x).toBe(1);
+    expect(canvas.scale.y).toBe(1);
+    canvas.zoomCamera(-10, -10);
+    expect(canvas.scale.x).not.toBe(1);
+    expect(canvas.scale.y).not.toBe(1);
+  });
+
+  it("should move camera", () => {
+    let canvas = new CanvasStore();
+    canvas.initialize(1000, 1000);
+    let x = 1.5 * RECT_W;
+    let y = 1.5 * RECT_H;
+    expect(canvas.camera.x).toEqual(x);
+    expect(canvas.camera.y).toEqual(y);
+    canvas.moveCamera(10, 10);
+    let scrollFactor = 1.5;
+    expect(canvas.camera.x).toBe(x + 10 * scrollFactor);
+    expect(canvas.camera.y).toBe(y + 10 * scrollFactor);
+  });
+
+  it("should zoom camera", () => {
+    let canvas = new CanvasStore();
+    canvas.initialize(1000, 1000);
+    canvas.movePointer(100, 100);
+    let z = 1000 / (2 * Math.tan((30 * Math.PI) / 180));
+    expect(canvas.camera.z).toEqual(z);
+    let oldScale = { ...canvas.scale };
+    canvas.zoomCamera(-10, -10);
+    let newW = 2 * (z - 100) * Math.tan(CAMERA_ANGLE);
+    let newH = 1 * newW;
+    let { x, y } = scaleWithAnchorPoint(
+      1100,
+      1100,
+      1.5 * RECT_W,
+      1.5 * RECT_H,
+      oldScale.x,
+      oldScale.y,
+      1000 / newW,
+      1000 / newH
+    );
+    expect(canvas.camera.x).toBe(x);
+    expect(canvas.camera.y).toBe(y);
+    expect(canvas.camera.z).toBe(z - 100);
   });
 });
